e89a3e167cf43f11fdfbf2b083a5e322dce65270
applemuffinhead <hkv5@hi.is>
2023-02-25 21:16:32 +0000

    Laga aðeins til

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,7 +1,7 @@
 -- Active: 1677343443752@@localhost@5432@vef2-verk2-2023@public
-INSERT INTO events (id, name, slug, description) VALUES (1, 'Forritarahittingur í febrúar', 'forritarahittingur-i-februar', 'Forritarar hittast í febrúar og forrita saman eitthvað frábært.');
-INSERT INTO events (id, name, slug, description) VALUES (2, 'Hönnuðahittingur í mars', 'honnudahittingur-i-mars', 'Spennandi hittingur hönnuða í Hönnunarmars.');
-INSERT INTO events (id, name, slug, description) VALUES (3, 'Verkefnastjórahittingur í apríl', 'verkefnastjorahittingur-i-april', 'Virkilega vel verkefnastýrður hittingur.');
+INSERT INTO events (id, name, slug, description, location, url) VALUES (1, 'Forritarahittingur í febrúar', 'forritarahittingur-i-februar', 'Forritarar hittast í febrúar og forrita saman eitthvað frábært.', null, null);
+INSERT INTO events (id, name, slug, description, location, url) VALUES (2, 'Hönnuðahittingur í mars', 'honnudahittingur-i-mars', 'Spennandi hittingur hönnuða í Hönnunarmars.', null, null);
+INSERT INTO events (id, name, slug, description, location, url) VALUES (3, 'Verkefnastjórahittingur í apríl', 'verkefnastjorahittingur-i-april', 'Virkilega vel verkefnastýrður hittingur.', null, null);
 
 INSERT INTO registrations (name, comment, event) VALUES ('Forvitinn forritari', 'Hlakka til að forrita með ykkur', 1);
 INSERT INTO registrations (name, comment, event) VALUES ('Jón Jónsson', null, 1);
